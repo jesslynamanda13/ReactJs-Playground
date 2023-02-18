45650f1513c4bc170063954f730c2f2432910b76
jesslynamanda13 <jesslynmulyawan@gmail.com>
2023-02-18 10:35:44 +0700

    first commit

---
+++ src/Test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Test() {
+  return (
+    <div className="App">
+     
+        <p>Hello</p>
+
+    </div>
+  );
+}
+
+export default Test;
